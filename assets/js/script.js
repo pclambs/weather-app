28f29bd92f19af9917fe48c4377f57d5eff1752b
deepwubs <pchucklambs@gmail.com>
2023-07-15 16:57:37 -0500

    append weatherData to dom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,43 @@
-var apiKey = '94e5456c3d04b880e4753e8fde1884d6'
+var apiKey = '94e5456c3d04b880e4753e8fde1884d6';
+
 var btn = document.querySelector("#btn-search");
+
 var containerPreviousSerches = document.querySelector("#previous-searches");
 
+var containerCurrent = document.querySelector("#target-city");
+
+var containerForecast = document.querySelector("#info-city");
+
 function renderElements(cityName, weatherData) {
-    // create a title with city name
-    var h2 = document.createElement('h2')
-    h2.innerTesxt = cityName
-    var ul = document.createElement('ul')
-    var li1 = document.createElement('ul')
-    var li2 = document.createElement('ul')
-    var li3 = document.createElement('ul')
-    li1.innerText = "Temp: " + weatherData.main.temp
-    li2.innerText = "Humidity: " + weatherData.main.humidity
-    li3.innerText = "Hum: " + weatherData.main.humidity
-    ul.appendChild(li1)
-    ul.appendChild(li2)
-    ul.appendChild(li3)
-    document.body.appendChild(h2)
-    document.body.appendChild(ul)
+    // Clear any existing content
+    containerCurrent.innerText = cityName;
+    containerForecast.innerHTML = "";
+
+    // Create an unordered list to display weather information
+    var ul = document.createElement('ul');
+    var li1 = document.createElement('li');
+    var li2 = document.createElement('li');
+    var li3 = document.createElement('li');
+    var li4 = document.createElement('li');
+    var li5 = document.createElement('li');
+    li1.innerText = "Temp: " + weatherData.main.temp + '°F';
+    li2.innerText = "H: " + weatherData.main.temp_max + '°F';
+    li3.innerText = "L: " + weatherData.main.temp_min + '°F';
+    li4.innerText = "Wind: " + weatherData.wind.speed + 'mph';
+    li5.innerText = "Humidity: " + weatherData.main.humidity + '%';
+
+    ul.appendChild(li1);
+    ul.appendChild(li2);
+    ul.appendChild(li3);
+    ul.appendChild(li4);
+    ul.appendChild(li5);
+
+    // Append the unordered list to the info-city div
+    containerForecast.appendChild(ul);
+    // Code to display the current date in the header of the page.
+    var today = dayjs().format('MMMM D, YYYY');
+    
+    $('#today').text(today.format('MMMM D, YYYY'));
 }
 
 function getWeatherByCity(cityName) {
@@ -27,12 +47,9 @@ function getWeatherByCity(cityName) {
     })
     .then(function(weatherData) {
         renderElements(cityName, weatherData)
+        console.log(weatherData)
     })
 }
-
-getWeatherByCity('Milwaukee')
-
-
 
 // function listener on click button
 var search = function(event){
@@ -46,13 +63,9 @@ var search = function(event){
     else{
         console.log(textInput);
         // call function for api response
-        callApiFetch(textInput);
-
+        getWeatherByCity(textInput);
     }
-
 };
-
-
 
 // Add event listener to Searching button 
 btn.addEventListener("click", search);
